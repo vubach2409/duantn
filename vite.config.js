b32e4e9ab72752764a4934a02a6415091947bd34
Kai <kaido4492@gmail.com>
2024-11-15 00:02:37 +0700

    Add Warden configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            host: true,
+            hmr: {
+                host: env.VITE_APP_DOMAIN,
+                clientPort: 443,
+                protocol: 'wss',
+            },
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    };
 });
